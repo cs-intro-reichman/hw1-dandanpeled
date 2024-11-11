b2301275cbd79b60ddacdb4496eafb5a2e792ca8
Dan Peled <danpeled@Dans-Air.lan>
2024-11-11 09:58:12 +0200

    Pow method changes - rate/100

--- FVCalc.java
+++ FVCalc.java
@@ -7,7 +7,9 @@ public class FVCalc {
 		double rate = Double.parseDouble(args[1]);
 		int n = Integer.parseInt(args[2]);
 
-		double futureValue = (currentValue * Math.pow((1 + rate), n))/100; // Calculate the future value
+		System.out.println(1+(rate/100));
+
+		double futureValue = currentValue * Math.pow((1 + (rate/100)), n); // Calculate the future value
 
 		//Print output
 		String outputAnswer = "After " + n +" years, a $" + currentValue + " saved at " + rate + "% will yield $" + (int)futureValue;
